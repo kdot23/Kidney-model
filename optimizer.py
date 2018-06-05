7e2dd6212741fcb826b2e8f849714920b8d4144a
kdot23 <30609252+kdot23@users.noreply.github.com>
2018-06-05 15:42:28 -0500

    reviewed and added comments for my clarification

--- optimizer.py
+++ optimizer.py
@@ -4,32 +4,38 @@ from gurobipy import *
 with open("data.json",'r') as f:
     data = json.load(f)
 compat = data[0]
-imcompat = data[1]
+incompat = data[1]
 Igraph = data[2]
-Cgraph = data[3]
+CIgraph = data[3]
 T = len(compat)
 
 model = Model('Kideny Optimizer')
 #first entry is which compatible pair, second entry is incompatible pair or -1 for self matching
-#cMatches are compatible to incompatible
-cMatches = model.addVars(range(T), range(-1, T), vtype = GRB.BINARY, name="CMatches")
+#ciMatches are compatible to incompatible or compatible to themselves (-1)
+ciMatches = model.addVars(range(T), range(-1, T), vtype = GRB.BINARY, name="CIMatches")
 #iMatches are incompatible to incompatible
 iMatches = model.addVars(range(T), range(T), vtype = GRB.BINARY, name = "IMatches")
 
-#compatible pair can only match with one incompatible pair
-model.addConstrs((quicksum(cMatches[i,j] for j in range(-1,T))  <= 1 for i in range(T)), "Compat Matches")
-#incompatible pair can only match with either one incompatible pair or one compatible pair
-model.addConstrs((quicksum(iMatches[i,j] for j in range(T)) + quicksum(cMatches[j,i] for j in range(T)) <= 1 for i in range(T)), "Imcompat Matches")
+#compatible pair can only match with up to one incompatible pair or themselves
+model.addConstrs((quicksum(ciMatches[i,j] for j in range(-1,T))  <= 1 for i in range(T)), "Compat to Incompat Matches")
+#incompatible pair can only match with up to one incompatible pair or one compatible pair
+model.addConstrs((quicksum(iMatches[i,j] for j in range(T)) + quicksum(ciMatches[j,i] for j in range(T)) <= 1 for i in range(T)), "Incompat Matches")
 
-model.addConstrs((iMatches[i,j] == iMatches[j,i] for i in range(T) for j in range(T)), "Symetry")
+#model.addConstrs((iMatches[i,j] == iMatches[j,i] for i in range(T) for j in range(T)), "Symmetry")
 
-model.addConstrs((cMatches[i,j]*imcompat[j][2] >= cMatches[i,j]*compat[i][2] for i in range(T) for j in range(T)), " Compat Selfishness")
+#compatible will only exchange if they have a higher quality score due to the exchange
+model.addConstrs((ciMatches[i,j]*incompat[j][2] >= ciMatches[i,j]*compat[i][2] for i in range(T) for j in range(T)), " Compat Selfishness")
 
-model.addConstrs((Cgraph[i][j] >= cMatches[i,j] for i in range(T) for j in range(T)), "Compatible Compatibility")
+#an edge between C and I can only exist if the pairs are compatible
+model.addConstrs((CIgraph[i][j] >= ciMatches[i,j] for i in range(T) for j in range(T)), "Compatible to Incompatible Compatibility")
 
+#an edge between two Is can only exist if the pairs are compatible 
 model.addConstrs((Igraph[i][j] >= iMatches[i,j] for i in range(T) for j in range(T)), "Incompatible Compatibility")
 
-obj = quicksum((compat[i][2]+imcompat[j][2])*cMatches[i,j] for i in range(T) for j in range(T)) + quicksum(compat[i][2]*cMatches[i,-1] for i in range(T)) + quicksum(imcompat[i][2]*iMatches[i,j] for i in range(T) for j in range(T))
+#substitute this line for line 24 to allow for arbitrary length cycles in incompatible pool
+model.addConstrs((quicksum(iMatches[i,j] for j in range(T)) == quicksum(iMatches[j,i] for j in range(T)) for i in range(T)), "Give one to get one")
+
+obj = quicksum((compat[i][2]+incompat[j][2])*ciMatches[i,j] for i in range(T) for j in range(T)) + quicksum(compat[i][2]*ciMatches[i,-1] for i in range(T)) + quicksum(incompat[i][2]*iMatches[i,j] for i in range(T) for j in range(T))
 model.setObjective(obj, GRB.MAXIMIZE) 
 
 model.optimize()
--- simpleKidneyDataGen.py
+++ simpleKidneyDataGen.py
@@ -1,7 +1,9 @@
+#generate data that can be used to create 2 cycle loops (or infinite size loops or incompatible pairs)
 import random
 import json
 
-T = 100
+T = 300
+#generate list of compatible pairs
 compat = []
 #d=0 is more compatible and d=1 is less compatible, r=0 is easy to match, r=1 hard to match
 for i in range(T):
@@ -16,7 +18,8 @@ for i in range(T):
     q = random.expovariate(.2)
     compat.append((d,r,q))
 print compat
-    
+
+#generate list of incompatible pairs  
 incompat = []        
 for i in range(T):
     if random.random() < .5:
@@ -31,6 +34,7 @@ for i in range(T):
     incompat.append((d,r,q))
 print incompat
 
+#create matrix with 1 representing an edge between the incompatible pairs
 Igraph = []
 for i in range(T):
     Igraph.append([])
@@ -49,20 +53,35 @@ for i in range(T):
             Igraph[i].append(int(p < .01))
 print Igraph
 
-Cgraph = []
+#create matrix with 1 representing an edge between compatible and incompatible pair
+CIgraph = []
 for i in range(T):
-    Cgraph.append([])
+    CIgraph.append([])
     for j in range(T):
         p = random.random()
+        p2 = random.random()
+        v1 = 0
+        v2 = 0
         if not compat[i][0] and not incompat[j][1]:
-            Cgraph[i].append(int(p < .04))
+            v1 = int(p < .04)
         if not compat[i][0] and incompat[j][1]:
-            Cgraph[i].append(int(p < .03))
+            v1 = int(p < .03)
         if compat[i][0] and not incompat[j][1]:
-            Cgraph[i].append(int(p < .02))
+            v1 = int(p < .02)
         if compat[i][0] and incompat[j][1]:
-            Cgraph[i].append(int(p < .01))
-print Cgraph
+            v1 = int(p < .01)
+        #add v2 because the graph is undirected
+        if not incompat[j][0] and not compat[i][1]:
+            v2 = int(p2 < .04)
+        if not incompat[j][0] and compat[i][1]:
+            v2 = int(p2 < .03)
+        if incompat[j][0] and not compat[i][1]:
+            v2 = int(p2 < .02)
+        if incompat[j][0] and compat[i][1]:
+            v2 = int(p2 < .01)
+        CIgraph[i].append(v1&v2)
+            
+print CIgraph
 
 with open('data.json', 'w') as f:
-    f.write(json.dumps((compat, incompat, Igraph, Cgraph)))
+    f.write(json.dumps((compat, incompat, Igraph, CIgraph)))
