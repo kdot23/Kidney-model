e226ed1b6ee438653ae9817cb0a322c5cc2d2576
kdot23 <30609252+kdot23@users.noreply.github.com>
2018-06-06 10:33:38 -0500

    added 2 and 3 cycles

--- optimizer2.py
+++ optimizer2.py
@@ -14,11 +14,66 @@ with open("data.json",'r') as f:
 compat = data[0]
 incompat = data[1]
 Igraph = data[2]
-Cgraph = data[3]
+CIgraph = data[3]
 ICgraph = data[4]
 T = len(compat)
 
+def twoCycle(Igraph, CIgraph, ICgraph):
+    twoCycles = {}
+    for i in range(T):
+        for j in range(T):
+            #if an edge exists between incompatible pair i and j 
+            if Igraph[i][j] and Igraph[j][i]:
+                #value at (i,j) is the quality of both exchanges
+                twoCycles[(i,j)] = incompat[i][2] + incompat[j][2]
+            if CIgraph[i][j] and ICgraph[j][i] and incompat[j][2] > compat[i][2]:
+                twoCycles[(i+T,j)] = compat[i][2] + incompat[j][2]
+        twoCycles[(i+T,i+T)] = compat[i][2]
+    return twoCycles
+def threeCycle(Igraph, CIgraph, ICgraph):
+    threeCycles = {}
+    for i in range(T):
+        for j in range(T):
+            if Igraph[i][j]:
+                for w in range(T):
+                    if w >= i or w >= j:
+                        break
+                    if Igraph[j][w] and Igraph[w][i]:
+                        threeCycles[(i,j,w)] = incompat[i][2] + incompat[j][2] + incompat[w][2]
+            if CIgraph[i][j]:
+                for w in range(T):
+                    if w >= j:
+                        break
+                    if Igraph[j][w] and ICgraph[w][i]:
+                        threeCycles[(i+T,j,w)] = compat[i][2] + incompat[j][2] + incompat[w][2]
+    return threeCycles
+                        
+
+twoCycles = twoCycle(Igraph, CIgraph, ICgraph)
+threeCycles = threeCycle(Igraph, CIgraph, ICgraph)
+    
+
+
+
 model = Model('Kideny Optimizer')
+model2 = Model('Kidney Optimizer2')
+c= {}
+for cycle in twoCycles:
+    c[cycle] = model2.addVar(vtype=GRB.BINARY, name="c_%s" % str(cycle))
+for cycle in threeCycles:
+    c[cycle] = model2.addVar(vtype=GRB.BINARY, name="c_%s" % str(cycle))
+model2.update()
+
+for v in range(2*T):
+  constraint = []
+  for cycle in c:
+      if (v in cycle):
+          constraint.append(c[cycle])
+  if constraint:
+      model2.addConstr( quicksum( con for con in constraint ) <= 1 , name="v%d" % v)
+model2.setObjective( quicksum( c[cycle] * twoCycles[cycle] for cycle in twoCycles ) + quicksum(c[cycle] * threeCycles[cycle] for cycle in threeCycles ), GRB.MAXIMIZE )
+
+
 #first entry is which compatible pair, second entry is incompatible pair or -1 for self matching
 #cMatches are compatible to incompatible
 cMatches = model.addVars(range(T), range(-1, T), vtype = GRB.BINARY, name="CMatches")
@@ -39,7 +94,7 @@ model.addConstrs((quicksum(cMatches[i,j] for j in range(T)) == quicksum(icMatche
 
 model.addConstrs((cMatches[i,j]*incompat[j][2] >= cMatches[i,j]*compat[i][2] for i in range(T) for j in range(T)), " Compat Selfishness")
 
-model.addConstrs((Cgraph[i][j] >= cMatches[i,j] for i in range(T) for j in range(T)), "Compatible Compatibility")
+model.addConstrs((CIgraph[i][j] >= cMatches[i,j] for i in range(T) for j in range(T)), "Compatible Compatibility")
 
 model.addConstrs((Igraph[i][j] >= iMatches[i,j] for i in range(T) for j in range(T)), "Incompatible Compatibility")
 
@@ -48,7 +103,7 @@ model.addConstrs((ICgraph[i][j] >= icMatches[i,j] for i in range(T) for j in ran
 obj = quicksum(compat[i][2]*cMatches[i,j] for i in range(T) for j in range(T)) + quicksum(incompat[j][2]*icMatches[j,i] for i in range(T) for j in range(T)) + quicksum(compat[i][2]*cMatches[i,-1] for i in range(T)) + quicksum(incompat[i][2]*iMatches[i,j] for i in range(T) for j in range(T))
 model.setObjective(obj, GRB.MAXIMIZE) 
 
-model.optimize()
-for v in model.getVars():
+model2.optimize()
+for v in model2.getVars():
     if v.X != 0:
         print str(v.Varname) + ' ' + str(v.X)
