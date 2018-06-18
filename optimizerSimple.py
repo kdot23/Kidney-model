7bdef68bc3c14af9bc50a24a1a988bfe5c7e9035
Kelsey Lieberman <kal7424@truman.edu>
2018-06-18 16:12:06 -0500

    fixed results for experimenting on optimizerSimple

--- optimizerSimple.py
+++ optimizerSimple.py
@@ -8,6 +8,7 @@ Created on Fri Jun 15 11:05:58 2018
 import json
 import argparse
 from gurobipy import *
+import numpy as np
 
 parser = argparse.ArgumentParser(description="Optimizes Kidney Exchange given by input file")
 parser.add_argument('--inputFile', nargs='?', help="JSON File to be used as input. List of number of \
@@ -15,39 +16,66 @@ parser.add_argument('--inputFile', nargs='?', help="JSON File to be used as inpu
 parser.add_argument('--quality', action='store_true', help="Optimize for quality")
 parser.add_argument('-i', '--inputDir', nargs='?', default='data', help='input directory to look for data files')
 args=parser.parse_args()
-
-if not args.inputDir:
+data = []
+if args.inputFile:
     with open (args.inputFile, 'r') as f:
-        data = json.load(f)
+        data.append(json.load(f))
 else:
     for fn in os.listdir(args.inputDir):
         if fn == '.DS_Store': continue
         with open(args.inputDir+'/'+fn, 'r') as f:
             data.append(json.load(f))
 
-num_incompat = data[0]
-num_compat = data[1]
-num_pairs = num_incompat + num_compat
-matches = data[2]
-T = num_compat
-model = Model('Kideny Optimizer')
-matchVars = {}
-for i in range(num_pairs):
-    for j in range(num_incompat+1):
-        if matches[i][j] != 0:
-            matchVars[(i,j)] = model.addVar(vtype = GRB.CONTINUOUS, lb = 0, ub=1,  name = "match_" + str((i,j)))
-
-model.addConstrs((quicksum(matchVars[t,i] for i in range(num_incompat+1) if (t,i) in matchVars) <= 1 \
-                  for t in range(num_pairs)), "Only match with one pair")
-
-model.addConstrs((quicksum(matchVars[t,i] for t in range(num_pairs) if (t,i) in matchVars) + \
-                  quicksum(matchVars[i+T-1,j] for j in range(1,num_incompat+1) if (i+T-1,j) in matchVars) <= 1 for i in range(1,num_incompat+1)), \
-                    "undirected graph")
-#add symmetry constraint??
-if (args.quality):
-    obj = quicksum(matchVars[i,j]*matches[i][j] for i in range(num_pairs) for j in range(num_incompat+1) if (i,j) in matchVars)
-else:
-    obj = quicksum(matchVars[i,j] for i in range(num_pairs) for j in range(num_incompat+1) if (i,j) in matchVars)
+pastData = []
+for d in data:    
+    num_incompat = d[0]
+    num_compat = d[1]
+    num_pairs = num_incompat + num_compat
+    matches = d[2]
+    T = num_compat
+    model = Model('Kideny Optimizer')
+    matchVars = {}
+    for i in range(num_pairs):
+        for j in range(num_incompat+1):
+            if matches[i][j] != 0:
+                matchVars[(i,j)] = model.addVar(vtype = GRB.CONTINUOUS, lb = 0, ub=1,  name = "match_" + str((i,j)))
     
-model.setObjective(obj, GRB.MAXIMIZE) 
-model.optimize()
+    model.addConstrs((quicksum(matchVars[t,i] for i in range(num_incompat+1) if (t,i) in matchVars) <= 1 \
+                      for t in range(num_pairs)), "Only match with one pair")
+    
+    model.addConstrs((quicksum(matchVars[t,i] for t in range(num_pairs) if (t,i) in matchVars) + \
+                      quicksum(matchVars[i+T-1,j] for j in range(1,num_incompat+1) if (i+T-1,j) in matchVars) <= 1 \
+                      for i in range(1,num_incompat+1)), "undirected graph")
+    #add symmetry constraint??
+    if (args.quality):
+        obj = quicksum(matchVars[i,j]*matches[i][j] for i in range(num_pairs) for j in range(num_incompat+1) if (i,j) in matchVars)
+    else:
+        obj = quicksum(matchVars[i,j] for i in range(num_pairs) for j in range(num_incompat+1) if (i,j) in matchVars)
+        
+    model.setObjective(obj, GRB.MAXIMIZE) 
+    model.optimize()
+    
+    num_matches = 0
+    for v in model.getVars():
+        if v.X != 0:
+            num_matches += 1
+    quality = 0
+    for i in range(num_pairs):
+        for j in range(num_incompat + 1):
+            if (i,j) in matchVars:
+                quality += matchVars[i,j].X*matches[i][j]
+    pastData.append((obj.getValue(), quality, num_matches, quality/num_matches))
+avgs = np.mean(pastData, axis=0)
+stdevs = np.std(pastData, axis=0)
+s = ''
+results = ''
+for std in stdevs:
+    s += str(std)+"\t"
+s += "\n"
+results = s+"\n\n\n"+results
+s = ''
+for a in avgs:
+    s += str(a)+"\t"
+s+="\n"
+results = s+results
+print results
