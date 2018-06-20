46e349a245c2c06d5df58a72adf7111feda0fdef
wacky556 <william.a.macke@gmail.com>
2018-06-20 10:16:56 -0500

    added stuff for comparing prediction on different populations

--- trainOnline.py
+++ trainOnline.py
@@ -4,10 +4,13 @@ from sklearn.preprocessing import PolynomialFeatures
 import tensorflow as tf
 from sklearn.model_selection import cross_val_score
 import argparse
+import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-i','--input', default='trainData.json')
-parser.add_argument('-o', default = "results.csv")
+parser.add_argument('--inputFile', default='trainData.json')
+parser.add_argument('-i','--input', default='./')
+parser.add_argument('--diffPops', action='store_true')
+parser.add_argument('-o', '--output')
 parser.add_argument('--coef', action='store_true', help = "boolean for coefficients for linear model")
 args = parser.parse_args()
 
@@ -15,11 +18,10 @@ args = parser.parse_args()
 varNames = ["bloodTypePatient", "bloodTypeDonor", "donor_afam", "donor_age", "donor_sex", "donor_cig_use", \
             "rec_sex", "donor_weight", "rec_weight", "donor_bmi"]
 varsUsed = [0, 1, 3, 5, 8, 9]
-num_chunks = 10
 
-inputFile = args.input
+inputFile = args.inputFile
 
-with open(inputFile, 'r') as f:
+with open(args.input+'/'+inputFile, 'r') as f:
     data = json.load(f)
 
 values = []
@@ -43,26 +45,59 @@ if args.coef:
 results = ''
 """
 if args.coef:
-    with open(args.o, 'w') as f:
-        f.write(results)
+    if args.output:
+        with open(args.output, 'w') as f:
+            f.write(results)
+    else:
+        print results
     exit()
-chunk_size = len(data) / num_chunks
-for i in range(num_chunks):
-    for j in range(1,5):
-        test_chunk_val = values[i*chunk_size: (i+1)*chunk_size]
-        test_chunk_lab = labels[i*chunk_size: (i+1)*chunk_size]
-        train_chunk_val = values[: i*chunk_size] + values[(i+1)*chunk_size:]
-        train_chunk_lab = labels[: i*chunk_size] + labels[(i+1)*chunk_size:]
-        poly = PolynomialFeatures(degree=j)
-        X = poly.fit_transform(train_chunk_val)
-        LR = linear_model.LinearRegression()
-        LR.fit(X, train_chunk_lab)
-        X2 = poly.fit_transform(test_chunk_val)
-        results +=  str(LR.score(X2, test_chunk_lab))+"\t"
-    results += "\n"
+
+if not args.diffPops:
+    num_chunks = 10
+    chunk_size = len(data) / num_chunks
+    for i in range(num_chunks):
+        for j in range(1,5):
+            test_chunk_val = values[i*chunk_size: (i+1)*chunk_size]
+            test_chunk_lab = labels[i*chunk_size: (i+1)*chunk_size]
+            train_chunk_val = values[: i*chunk_size] + values[(i+1)*chunk_size:]
+            train_chunk_lab = labels[: i*chunk_size] + labels[(i+1)*chunk_size:]
+            poly = PolynomialFeatures(degree=j)
+            X = poly.fit_transform(train_chunk_val)
+            LR = linear_model.LinearRegression()
+            LR.fit(X, train_chunk_lab)
+            X2 = poly.fit_transform(test_chunk_val)
+            results +=  str(LR.score(X2, test_chunk_lab))+"\t"
+        results += "\n"
+else:
+    for fn in os.listdir(args.input):
+        if fn == inputFile or fn == '.DS_Store': continue
+        data = None
+        with open(args.input+'/'+fn, 'r') as f:
+            data = json.load(f)
+        
+        testValues = []
+        testLabels = []
+        
+        for d in data:
+            demo = d[0]
+            testValues.append([demo[v] for v in varsUsed])
+            testLabels.append(d[1])
+        for i in range(1,5):
+            poly = PolynomialFeatures(degree=i)
+            X = poly.fit_transform(values)
+            LR = linear_model.LinearRegression()
+            LR.fit(X, labels)
+            X2 = poly.fit_transform(testValues)
+            results += str(LR.score(X2, testLabels)) + "\t"
+        results += "\n"
+
+
     
-with open(args.o, 'w') as f:
-    f.write(results)
+if args.output:
+    with open(args.output, 'w') as f:
+        f.write(results)
+else:
+    print results
 
 
 """
