38c977980043ada4941f1614f21fcff76327dd4a
wacky556 <william.a.macke@gmail.com>
2018-06-21 10:29:25 -0500

    added a bunch of stuff to trainOnline

--- trainOnline.py
+++ trainOnline.py
@@ -1,41 +1,53 @@
 import json
 from sklearn import linear_model
 from sklearn.preprocessing import PolynomialFeatures
-import tensorflow as tf
 from sklearn.model_selection import cross_val_score
 import argparse
 import os
+import random
+import pickle
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--inputFile', default='trainData.json')
-parser.add_argument('-i','--input', default='./')
-parser.add_argument('--diffPops', action='store_true')
-parser.add_argument('-o', '--output')
+parser.add_argument('--inputFiles', default=['trainData.json'], nargs='+', help='list of files to use as input, \
+if diffpops is present these will be used as train files and all other files in input directory will be used as test files, \
+otherWise n fold cross validation is performed')
+parser.add_argument('-i','--input', default='./', help='input directory to be used')
+parser.add_argument('--diffPops', action='store_true', help='flag for whether or not to use cross validation, see inputFiles flag')
+parser.add_argument('-d', '--degree', default=[1], type=int, nargs='+', help='types of polynomials to use while training')
+parser.add_argument('-v', '--useVars', default=[0,1,3,5,8,9], type=int, nargs='+', help='variables to train off of' )
+parser.add_argument('-o', '--output', help='output File')
+parser.add_argument('-n', '--numChunks', default=10, type=int, 'n for n-fold cross validation')
 parser.add_argument('--coef', action='store_true', help = "boolean for coefficients for linear model")
+parser.add_argument('--saveModel', help='optional flag to save model, argument is stem of file(e.g. if file is given, then model of degree 3 \
+        will be saved to file3.dat)')
 args = parser.parse_args()
 
 
 varNames = ["bloodTypePatient", "bloodTypeDonor", "donor_afam", "donor_age", "donor_sex", "donor_cig_use", \
             "rec_sex", "donor_weight", "rec_weight", "donor_bmi"]
-varsUsed = [0, 1, 3, 5, 8, 9]
 
-inputFile = args.inputFile
+data = []
+for fn in args.inputFiles:
+    with open(args.input+'/'+fn, 'r') as f:
+        data += json.load(f)
 
-with open(args.input+'/'+inputFile, 'r') as f:
-    data = json.load(f)
+random.shuffle(data)
 
 values = []
 labels = []
 
 for d in data:
     demo = d[0]
-    values.append([demo[v] for v in varsUsed])
+    values.append([demo[v] for v in args.useVars])
     labels.append(d[1])
 results = ''
+
+
+
 if args.coef:
     LR = linear_model.LinearRegression()
     LR.fit(values, labels)
-    for i in range(len(varsUsed)):
+    for i in range(len(args.useVars)):
         results += str(LR.coef_[i]) + "\t"
     results += "\n"
 """
@@ -53,10 +65,9 @@ if args.coef:
     exit()
 
 if not args.diffPops:
-    num_chunks = 10
-    chunk_size = len(data) / num_chunks
-    for i in range(num_chunks):
-        for j in range(1,5):
+    chunk_size = len(data) / args.numChunks
+    for i in range(args.numChunks):
+        for j in args.degree:
             test_chunk_val = values[i*chunk_size: (i+1)*chunk_size]
             test_chunk_lab = labels[i*chunk_size: (i+1)*chunk_size]
             train_chunk_val = values[: i*chunk_size] + values[(i+1)*chunk_size:]
@@ -70,7 +81,7 @@ if not args.diffPops:
         results += "\n"
 else:
     for fn in os.listdir(args.input):
-        if fn == inputFile or fn == '.DS_Store': continue
+        if fn in args.inputFiles or fn == '.DS_Store': continue
         data = None
         with open(args.input+'/'+fn, 'r') as f:
             data = json.load(f)
@@ -80,9 +91,9 @@ else:
         
         for d in data:
             demo = d[0]
-            testValues.append([demo[v] for v in varsUsed])
+            testValues.append([demo[v] for v in args.useVars])
             testLabels.append(d[1])
-        for i in range(1,5):
+        for i in args.degree:
             poly = PolynomialFeatures(degree=i)
             X = poly.fit_transform(values)
             LR = linear_model.LinearRegression()
@@ -98,6 +109,15 @@ if args.output:
         f.write(results)
 else:
     print results
+
+if args.saveModel:
+    for i in args.degree:
+        poly = PolynomialFeatures(degree=i)
+        X = poly.fit_transform(values)
+        LR = linear_model.LinearRegression()
+        LR.fit(X,labels)
+        with open(args.saveModel+str(i)+'.dat', 'wb') as f:
+            pickle.dump(LR, f)
 
 
 """
