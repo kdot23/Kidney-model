16c6699ff15437d2c65df44cdcc7f0338107f28b
Kelsey Lieberman <kal7424@truman.edu>
2018-06-27 15:08:39 -0500

    standardized flags

--- 2Cycle/optimizerSimple.py
+++ 2Cycle/optimizerSimple.py
@@ -9,21 +9,17 @@ from gurobipy import *
 import numpy as np
 
 parser = argparse.ArgumentParser(description="Optimizes Kidney Exchange given by input file")
-parser.add_argument('--inputFile', nargs='?', help="JSON File to be used as input. List of number of \
+parser.add_argument('--inputFiles', nargs='+', default = ["data.dat"], help="list of .dat files to be used as input. List of number of \
                     incompatible pairs, number of compatible pairs, and list of all possible pairs with donor, recipient, egs")
 parser.add_argument('--quality', action='store_true', help="Optimize for quality")
-parser.add_argument('-i', '--inputDir', nargs='?', default='data', help='input directory to look for data files')
+parser.add_argument('-o', '--output', default='data.dat')
 args=parser.parse_args()
-data = []
 
-if args.inputFile:
-    with open (args.inputFile, 'rb') as f:
+data = []
+for fn in args.inputFiles:
+    with open (fn, 'rb') as f:
         data.append(pickle.load(f))
-else:
-    for fn in os.listdir(args.inputDir):
-        if fn == '.DS_Store': continue
-        with open(args.inputDir+'/'+fn, 'rb') as f:
-            data.append(pickle.load(f))
+        
 def COUNT(v):
     if v[1] == 0:
         return 1
@@ -56,28 +52,14 @@ for d in data:
   
     model.setObjective(obj, GRB.MAXIMIZE) 
     model.optimize()
+            
+    quality = sum(matchVars[v].X*matches[v] for v in matchVars)
+    count = sum(COUNT(v)*matchVars[v].X for v in matchVars)
     
-    num_matches = 0
-    for v in model.getVars():
-        if v.X != 0:
-            num_matches += 1
-    quality = 0
-    for i in range(num_pairs):
-        for j in range(num_incompat + 1):
-            if (i,j) in matchVars:
-                quality += matchVars[i,j].X*matches[i][j]
-    pastData.append((obj.getValue(), quality, num_matches, quality/num_matches))
-avgs = np.mean(pastData, axis=0)
-stdevs = np.std(pastData, axis=0)
-s = ''
-results = ''
-for std in stdevs:
-    s += str(std)+"\t"
-s += "\n"
-results = s+"\n\n\n"+results
-s = ''
-for a in avgs:
-    s += str(a)+"\t"
-s+="\n"
-results = s+results
-print results
+    results += quality + "\t" + count + "\n"
+
+if args.output:
+    with open(args.output, 'w') as f:
+        f.write(results)
+else:
+    print results
