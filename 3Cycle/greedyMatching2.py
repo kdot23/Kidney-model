c8af43e5d3c697d2baebf3670718fd11dbb240d0
wacky556 <william.a.macke@gmail.com>
2018-10-15 10:34:45 -0500

    Added code for performing new greedy baseline suggested by CJ in file greedyMatching2.py in both 2 cycle and 3 cycle folders.

---
+++ 3Cycle/greedyMatching2.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+import json
+import pickle
+import argparse
+from gurobipy import *
+import numpy as np
+from sets import Set
+import os
+
+parser = argparse.ArgumentParser(description="Optimizes Kidney Exchange given by input file using a simple greedy algorithm")
+parser.add_argument('--inputFiles', nargs='+', default = ["data.dat"], help="List of .dat files to be used as input. List of number of \
+                    incompatible pairs, number of compatible pairs, list of quality(egs) of all possible pairs \
+                    and demographic information. File created in KidneyDataGen")
+parser.add_argument('--quality', action='store_true', help="Optimize for quality")
+parser.add_argument('-o', '--output', help='write results to this file (.csv)')
+parser.add_argument('--agents', help='output the quality of each agent to this file (.csv)')
+parser.add_argument("-n", type = int, default = 2, help = "max number of connections incompatibles can be matched to and still removed from pool")
+parser.add_argument("--graph", help = "output a graphviz representation")
+args=parser.parse_args()
+
+graph_colors = ["red", "blue", "green", "black"]
+
+def COUNT(v):
+    if v[2] != 0:
+        return 3
+    if v[1] != 0:
+        return 2
+    return 1
+
+def getBloodTypes(demo):
+    bd,br=0,0
+    if demo[0]:
+        br = 0
+    elif demo[1]:
+        br = 1
+    elif demo[2]:
+        br = 2
+    elif demo[3]:
+        br = 3
+    if demo[4]:
+        bd = 0
+    elif demo[5]:
+        bd = 1
+    elif demo[6]:
+        bd = 2
+    elif demo[7]:
+        bd = 3
+    return br,bd
+
+
+def solveLP1Compat(T, K, matches, used_incompat, compat):
+    model = Model("match compat")
+    matchVars = {}
+    for m in matches:
+        if m[0] <= T and m[0] != compat:
+            continue
+        if m[0] > T and (m[0]-T) in used_incompat:
+            continue
+        if m[1] in used_incompat:
+            continue
+        if m[2] in used_incompat:
+            continue
+        matchVars[m] = model.addVar(vtype=GRB.BINARY, lb = 0, ub = 1, name="match"+str(m))
+
+    
+    model.addConstrs((quicksum(matchVars[t,i,j] for i in range(K+1) for j in range(K+1) if (t,i,j) in matchVars) <= 1 \
+                      for t in range(1,T+K+1)), "Only match with one pair")
+    
+    model.addConstrs((quicksum(matchVars[t,i,j] for t in range(1,T+K+1) for j in range(K+1) if (t,i,j) in matchVars) \
+                     + quicksum(matchVars[t,j,i] for t in range(1,T+K+1) for j in range(1, K+1) if (t,j,i) in matchVars)\
+                     + quicksum(matchVars[i+T,j,jp] for j in range(1,K+1) for jp in range(K+1) if (i+T,j,jp) in matchVars)\
+                     <= 1 for i in range(1,K+1)), "undirected graph")
+
+
+    if (args.quality):
+        obj = quicksum(matchVars[v]*matches[v] for v in matchVars)
+    else:
+        obj = quicksum(COUNT(v)*matchVars[v] for v in matchVars)
+    model.setObjective(obj, GRB.MAXIMIZE) 
+    model.optimize()
+    
+    for m in matchVars:
+        if m[0] == compat and round(matchVars[m].X) > 0:
+            return m
+results = ''
+agentInfo = '' #Pair id, time of match, get quality, get donor type, give quality, give recipient type
+dataIndex = 0
+
+for fn in args.inputFiles:
+    with open(fn, 'rb') as f:
+        d = pickle.load(f)
+    num_incompat = d[0]
+    num_compat = d[1]
+    num_pairs = num_incompat + num_compat
+    matches = d[3]
+    directed_matches = d[6]
+    T = num_compat
+    K = num_incompat
+    demo = d[4]
+    
+    quality = 0
+    count = 0
+    
+    graph = "digraph G { \n"
+    #greedy algorithm for compatible pairs by order of index
+    used_incompat = set()
+    for i in range(1,num_compat+1):
+        #max_index = max((v for v in matches if v[0] == i and v[1] not in used_incompat and v[2] not in used_incompat), key=matches.get)
+        max_index = solveLP1Compat(T,K, matches, used_incompat, i)
+        quality += matches[max_index]
+
+        count += COUNT(max_index)
+        if max_index[1] != 0:
+            used_incompat.add(max_index[1])
+        if max_index[2] != 0:
+            used_incompat.add(max_index[2])
+        
+        if max_index[1] == 0:
+            agentInfo += "C" + str(i) + "\t" + str(i) + "\t" + str(directed_matches[max_index[0],0]) + "\t" \
+           + "C" + "\t" + str(directed_matches[max_index[0],0]) + "\t" + "C" + "\n"
+            bt = getBloodTypes(demo[i])
+            graph += "edge [color="+graph_colors[bt[1]] + "];\n"
+            graph += "node [color="+graph_colors[bt[0]]+"];\n"
+            graph += "C" + str(i) + " -> C" + str(i) + ";\n"
+            
+        elif max_index[2] == 0:
+            agentInfo += "C" + str(i) + "\t" + str(i) + "\t" + str(directed_matches[max_index[1]+T,max_index[0]]) + "\t" \
+           + "I" + "\t" + str(directed_matches[max_index[0],max_index[1]+T]) + "\t" + "I" + "\n"
+            agentInfo += "I" + str(max_index[1]) + "\t" + str(i) + "\t" + str(directed_matches[max_index[0],max_index[1]+T]) + "\t" \
+           + "C" + "\t" + str(directed_matches[max_index[1]+T,max_index[0]]) + "\t" + "C" + "\n"
+           
+            bt1 = getBloodTypes(demo[i])
+            bt2 = getBloodTypes(demo[max_index[1] + T - 1])
+            graph += "edge [color="+graph_colors[bt1[1]] + "];\n"
+            graph += "C" + str(i) + " [color="+graph_colors[bt1[0]]+"];\n"
+            graph += "I" + str(max_index[1]-1) + " [color="+graph_colors[bt2[0]]+"];\n"
+            graph += "C" + str(i) + " -> I" + str(max_index[1]-1) + ";\n"
+            graph += "edge [color="+graph_colors[bt2[1]] + "];\n"
+            graph += "I" + str(max_index[1]-1) + " -> C" + str(i) + ";\n"
+
+        else:
+            agentInfo += "C" + str(i) + "\t" + str(i) + "\t" + str(directed_matches[max_index[2]+T,max_index[0]]) + "\t" \
+           + "I" + "\t" + str(directed_matches[max_index[0],max_index[1]+T]) + "\t" + "I" + "\n"
+            agentInfo += "I" + str(max_index[1]) + "\t" + str(i) + "\t" + str(directed_matches[max_index[0],max_index[1]+T]) + "\t" \
+           + "C" + "\t" + str(directed_matches[max_index[1]+T,max_index[2]+T]) + "\t" + "I" + "\n"
+            agentInfo += "I" + str(max_index[2]) + "\t" + str(i) + "\t" + str(directed_matches[max_index[1]+T,max_index[2]+T]) + "\t" \
+           + "I" + "\t" + str(directed_matches[max_index[2]+T,max_index[0]]) + "\t" + "C" + "\n"
+        
+    #gurobi optimization for remaining incompatible pairs
+    model = Model('Kideny Optimizer')
+    matchVars = {v:model.addVar(vtype = GRB.BINARY, lb = 0, ub=1,  name = "incompat_match_" + str(v)) for v in matches if v[0] > T and \
+            v[0]-T not in used_incompat and v[1] not in used_incompat and v[2] not in used_incompat}
+
+    model.addConstrs((quicksum(matchVars[t,i,j] for i in range(num_incompat+1) for j in range(num_incompat+1) if (t,i,j) in matchVars) <= 1 \
+                      for t in range(1,num_pairs+1)), "Only match with one pair")
+    
+    model.addConstrs((quicksum(matchVars[t,i,j] for t in range(1,num_pairs+1) for j in range(num_incompat+1) if (t,i,j) in matchVars) \
+                     + quicksum(matchVars[t,j,i] for t in range(1,num_pairs+1) for j in range(1, num_incompat+1) if (t,j,i) in matchVars)\
+                     + quicksum(matchVars[i+T,j,jp] for j in range(1,num_incompat+1) for jp in range(num_incompat+1) if (i+T,j,jp) in matchVars)\
+                     <= 1 for i in range(1,num_incompat+1)), "undirected graph")
+
+    if (args.quality):
+        obj = quicksum(matchVars[v]*matches[v] for v in matchVars)
+    else:
+        obj = quicksum(COUNT(v)*matchVars[v] for v in matchVars)
+
+
+    model.setObjective(obj, GRB.MAXIMIZE) 
+    model.optimize()
+    
+
+    for v in matchVars:
+        if round(matchVars[v].X) != 0:
+            quality += matches[v]
+            count += COUNT(v)
+            used_incompat.add(v[0]-T)
+            used_incompat.add(v[1])
+            if v[2] == 0:
+                 agentInfo += "I" + str(v[0]-T) + "\t" + str(T+1) + "\t" + str(directed_matches[v[1]+T,v[0]]) + "\t" \
+               + "I" + "\t" + str(directed_matches[v[0],v[1]+T]) + "\t" + "I" + "\n"
+                 agentInfo += "I" + str(v[1]) + "\t" + str(T+1) + "\t" + str(directed_matches[v[0],v[1]+T]) + "\t" \
+               + "I" + "\t" + str(directed_matches[v[1]+T,v[0]]) + "\t" + "I" + "\n"
+            else:
+                 used_incompat.add(v[2])
+                 agentInfo += "I" + str(v[0]-T) + "\t" + str(T+1) + "\t" + str(directed_matches[v[2]+T,v[0]]) + "\t" \
+               + "I" + "\t" + str(directed_matches[v[0],v[1]+T]) + "\t" + "I" + "\n"
+                 agentInfo += "I" + str(v[1]) + "\t" + str(T+1) + "\t" + str(directed_matches[v[0],v[1]+T]) + "\t" \
+               + "I" + "\t" + str(directed_matches[v[1]+T,v[2]+T]) + "\t" + "I" + "\n"
+                 agentInfo += "I" + str(v[2]) + "\t" + str(T+1) + "\t" + str(directed_matches[v[1]+T,v[2]+T]) + "\t" \
+               + "I" + "\t" + str(directed_matches[v[2]+T,v[0]]) + "\t" + "I" + "\n"
+               
+            bt1 = getBloodTypes(demo[v[0]-1])
+            bt2 = getBloodTypes(demo[v[1] + T - 1])
+            graph += "edge [color="+graph_colors[bt1[1]] + "];\n"
+            graph += "I" + str(v[0]) + " [color="+graph_colors[bt1[0]]+"];\n"
+            graph += "I" + str(v[1]) + " [color="+graph_colors[bt2[0]]+"];\n"
+            graph += "I" + str(v[0]) + " -> I" + str(v[1]) + ";\n"
+            graph += "edge [color="+graph_colors[bt2[1]] + "];\n"
+            graph += "I" + str(v[1]) + " -> I" + str(v[0]) + ";\n"
+
+    for i in range(1,num_incompat+1):
+        if i not in used_incompat:
+             agentInfo += "I" + str(i) + "\t" + str(T+2) + "\t" + str(0) + "\t" \
+        + "N" + "\t" + str(0) + "\t" + "N" + "\n"
+
+    graph += "}"
+    if args.graph:
+        with open(args.graph+str(dataIndex)+".gv", 'w') as f:
+            f.write(graph)
+        os.system('dot -Tpdf ' + args.graph + str(dataIndex) + ".gv -o " + args.graph + str(dataIndex) +".pdf")
+            
+    dataIndex += 1
+    
+    results += str(count) + "\t" + str(quality) + "\n"
+    print "greedy" + str(fn)
+
+if args.output:
+    with open(args.output, 'w') as f:
+        f.write(results)
+        
+else:
+    print results
+
+if args.agents:
+    with open(args.agents, 'w') as f:
+        f.write(agentInfo)
+    
