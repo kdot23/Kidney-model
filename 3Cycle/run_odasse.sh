caaceefec0acf5423f72b8aa4e1fe85934b4d0b4
Kelsey Lieberman <kal7424@truman.edu>
2019-01-18 09:22:14 -0600

    ran experiments

--- 3Cycle/run_odasse.sh
+++ 3Cycle/run_odasse.sh
@@ -5,15 +5,14 @@ do
 	train[$(($i-1))]=pop50_100/data$i.dat
 done
 
-python generateBeta.py --inputFiles $train -o train.csv --quality --graph_state 
+python generateBeta.py --inputFiles ${train[@]} -o train.csv --quality --graph_state 
 '
-
 declare -a pops
 for i in $(seq 101 500)
 do
 	pops[$(($i-1))]=pop50_100/data$i.dat
 done
-python onlineMatching.py --trainFiles train.csv --testFiles ${pops[@]} --quality -o odasse.csv --agents odasse_agents.csv --graph_state --lpRepeat
 
 python onlineMatching.py --trainFiles train.csv --testFiles ${pops[@]} --quality -o odasse.csv --agents odasse_agents.csv --graph_state --lpRepeat
+
 
