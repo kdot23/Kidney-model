4375125bd0e3e61f0db04f9c4c9374b086f1c241
Kelsey Lieberman <kal7424@truman.edu>
2019-01-18 09:26:46 -0600

    added more files

---
+++ 3Cycle/run_odasse2.sh
@@ -0,0 +1,19 @@
+: '
+declare -a train
+for i in $(seq 1 100)
+do
+	train[$(($i-1))]=pop50_100/data$i.dat
+done
+
+python generateBeta.py --inputFiles $train -o train.csv --quality --graph_state 
+'
+
+declare -a pops
+for i in $(seq 101 150)
+do
+	pops[$(($i-1))]=pop50_100/data$i.dat
+done
+#python onlineMatching.py --trainFiles train.csv --testFiles ${pops[@]} --quality -o odasse.csv --agents odasse_agents.csv --graph_state --lpRepeat
+
+python onlineMatching.py --trainFiles train.csv --testFiles ${pops[@]} --quality -o odasse2.csv --agents odasse2_agents.csv --fwd_proj
+
