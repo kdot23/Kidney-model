93aca55e951de33f459ebe68548a53e991e5dfd2
Kelsey Lieberman <kal7424@truman.edu>
2019-02-03 14:13:43 -0600

    results of odasse2 with pop 100_100

--- 3Cycle/gen_pops.sh
+++ 3Cycle/gen_pops.sh
@@ -1,6 +1,7 @@
 for i in {1..500}
 do
 	echo $i
-	python KidneyDataGen.py -K 100 -T 50 -o pop50_100/data$i.dat
+	python KidneyDataGen.py -K 50 -T 50 -o pop50_50/data$i.dat
+	python KidneyDataGen.py -K 100 -T 100 -o pop100_100/data$i.dat
 done
 
--- 3Cycle/run_odasse2.sh
+++ 3Cycle/run_odasse2.sh
@@ -1,19 +1,19 @@
-: '
+
 declare -a train
 for i in $(seq 1 100)
 do
-	train[$(($i-1))]=pop50_100/data$i.dat
+	train[$(($i-1))]=pop100_100/data$i.dat
 done
 
-python generateBeta.py --inputFiles $train -o train.csv --quality --graph_state 
-'
+python generateBeta.py --inputFiles ${train[@]} -o train100_100.csv --quality --graph_state 
+
 
 declare -a pops
 for i in $(seq 101 150)
 do
-	pops[$(($i-1))]=pop50_100/data$i.dat
+	pops[$(($i-1))]=pop100_100/data$i.dat
 done
 #python onlineMatching.py --trainFiles train.csv --testFiles ${pops[@]} --quality -o odasse.csv --agents odasse_agents.csv --graph_state --lpRepeat
 
-python onlineMatching.py --trainFiles train.csv --testFiles ${pops[@]} --quality -o odasse2.csv --agents odasse2_agents.csv --fwd_proj
+python onlineMatching.py --trainFiles train100_100.csv --testFiles ${pops[@]} --quality -o odasse2.csv --agents odasse2_agents.csv --fwd_proj 50
 
