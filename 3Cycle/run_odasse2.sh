58adc45f4b4242c39280ec9926f5419202fdc3bd
Kelsey Lieberman <kal7424@truman.edu>
2019-02-06 00:54:54 -0600

    train5)_100 added

--- 3Cycle/run_odasse2.sh
+++ 3Cycle/run_odasse2.sh
@@ -1,19 +1,19 @@
-
+'''
 declare -a train
 for i in $(seq 1 100)
 do
-	train[$(($i-1))]=pop100_100/data$i.dat
+	train[$(($i-1))]=pop50_100/data$i.dat
 done
 
-python generateBeta.py --inputFiles ${train[@]} -o train100_100.csv --quality --graph_state 
-
+python generateBeta.py --inputFiles ${train[@]} -o train50_100.csv --quality --graph_state 
+'''
 
 declare -a pops
 for i in $(seq 101 150)
 do
-	pops[$(($i-1))]=pop100_100/data$i.dat
+	pops[$(($i-1))]=pop50_100/data$i.dat
 done
 #python onlineMatching.py --trainFiles train.csv --testFiles ${pops[@]} --quality -o odasse.csv --agents odasse_agents.csv --graph_state --lpRepeat
 
-python onlineMatching.py --trainFiles train100_100.csv --testFiles ${pops[@]} --quality -o odasse2.csv --agents odasse2_agents.csv --fwd_proj 50
+python onlineMatching.py --trainFiles train50_100.csv --testFiles ${pops[@]} --quality -o odasse3_50_100.csv --agents odasse3_50_100_agents.csv --fwd_proj 50 --fwd_proj_repeat
 
