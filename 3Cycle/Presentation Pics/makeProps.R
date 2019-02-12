3a5875582e9d323899bdd10e8ee2a520119a9946
Kelsey Lieberman <kal7424@truman.edu>
2019-02-11 20:37:39 -0600

    graphs for paper

--- 3Cycle/Presentation Pics/makeProps.R
+++ 3Cycle/Presentation Pics/makeProps.R
@@ -1,22 +1,36 @@
-greedyA = read.csv('greedyDA.csv', header=FALSE, sep='\t')
-odasseA = read.csv('onlineLinearGraphAgents.csv', header=FALSE, sep='\t')
+greedyA = read.csv('fancy_greedy_DA.csv', header=FALSE, sep='\t')
+odasseA = read.csv('fwd_rep_odasse_DA.csv', header=FALSE, sep='\t')
 
-greedyA = subset(greedyA, grepl('I', V1))
-totalGreedy = nrow(greedyA)
-propG1 = nrow(subset(greedyA, V2 <=50))/totalGreedy
-propG2 = nrow(subset(greedyA, V2 ==51))/totalGreedy
-propG3 = nrow(subset(greedyA, V2 >51))/totalGreedy
+greedyO = subset(greedyA, grepl('I', V1) & V7 == 1)
+totalGreedyO = nrow(greedyO)
+propG_O_1 = nrow(subset(greedyO, V2 <=50))/totalGreedyO
+propG_O_2 = nrow(subset(greedyO, V2 ==51))/totalGreedyO
+propG_O_3 = nrow(subset(greedyO, V2 >51))/totalGreedyO
 
+greedyNO = subset(greedyA, grepl('I', V1) & V7 == 1)
+totalGreedyNO = nrow(greedyNO)
+propG_NO_1 = nrow(subset(greedyNO, V2 <=50))/totalGreedyNO
+propG_NO_2 = nrow(subset(greedyNO, V2 ==51))/totalGreedyNO
+propG_NO_3 = nrow(subset(greedyNO, V2 >51))/totalGreedyNO
 
-odasseA = subset(odasseA, grepl('I', V1))
-totalOdasse = nrow(odasseA)
-propO1 = nrow(subset(odasseA, V2 <=50))/totalOdasse
-propO2 = nrow(subset(odasseA, V2 ==51))/totalOdasse
-propO3 = nrow(subset(odasseA, V2 >51))/totalOdasse
+odasseO = subset(odasseA, grepl('I', V1) & V8 == 1)
+totalOdasseO = nrow(odasseO)
+propO_O_1 = nrow(subset(odasseO, V2 <=50))/totalOdasseO
+propO_O_2 = nrow(subset(odasseO, V2 ==51))/totalOdasseO
+propO_O_3 = nrow(subset(odasseO, V2 >51))/totalOdasseO
 
-props = cbind(c(propG1, propG2, propG3), c(propO1, propO2, propO3))
+odasseNO = subset(odasseA, grepl('I', V1) & V8 == 1)
+totalOdasseNO = nrow(odasseNO)
+propO_NO_1 = nrow(subset(odasseNO, V2 <=50))/totalOdasseNO
+propO_NO_2 = nrow(subset(odasseNO, V2 ==51))/totalOdasseNO
+propO_NO_3 = nrow(subset(odasseNO, V2 >51))/totalOdasseNO
+
+props = cbind(c(propG_O_1, propG_O_2, propG_O_3),  
+              c(propO_O_1, propO_O_2, propO_O_3), 
+              c(propG_NO_1, propG_NO_2, propG_NO_3),
+              c(propO_NO_1, propO_NO_2, propO_NO_3))
 
 
 par(mar=c(10,5,2,2), xpd=TRUE)
-barplot(props, col=c('lightblue2', 'palegreen', 'palegoldenrod'), names=c('Greedy', 'ODASSE'), ylab='Proportion of Matches', main='Proportions of Matches by Algorithm')
-legend('bottom',legend=c('Received Kidney at End','Received Kidney During Online Part','Did not Receive a Kidney'), fill=c('lightblue2', 'palegreen', 'palegoldenrod'), inset=c(-.4,-.4))
+barplot(props, col=c('ivory4', 'ivory3', 'ivory1'), names=c('Fancy Greedy O', 'fwd rep ODASSE O', 'Fancy Greedy NO', 'fwd rep ODASSE NO'), ylab='Proportion of Matches', main='Proportions of Matches by Algorithm and Recipient Blood Type')
+legend('bottom',legend=c('Online', 'End', 'Unmatched'), fill=c('ivory4', 'ivory3', 'ivory2'), inset=c(-.4,-.8), ncol=3)
--- 3Cycle/run_odasse2.sh
+++ 3Cycle/run_odasse2.sh
@@ -5,15 +5,14 @@ do
 	train[$(($i-1))]=pop50_100/data$i.dat
 done
 
-python generateBeta.py --inputFiles ${train[@]} -o train50_100.csv --quality --graph_state 
+python generateBeta.py --inputFiles ${train[@]} -o train50_100_2_7.csv --quality --graph_state 
 '''
-
 declare -a pops
-for i in $(seq 101 150)
+for i in $(seq 102 150)
 do
 	pops[$(($i-1))]=pop50_100/data$i.dat
 done
+
 #python onlineMatching.py --trainFiles train.csv --testFiles ${pops[@]} --quality -o odasse.csv --agents odasse_agents.csv --graph_state --lpRepeat
 
-python onlineMatching.py --trainFiles train50_100.csv --testFiles ${pops[@]} --quality -o odasse3_50_100.csv --agents odasse3_50_100_agents.csv --fwd_proj 50 --fwd_proj_repeat
-
+python onlineMatching.py --trainFiles train50_100.csv --testFiles ${pops[@]} --quality -o results2_7/odasse3_50_100_10_2.csv --agents results2_7/odasse3_50_100_10_2agents.csv --fwd_proj 10 --fwd_proj_repeat
