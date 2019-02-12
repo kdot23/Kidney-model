49ba43f9b7bb93be8691aac41e6f157e09305a81
Kelsey Lieberman <kal7424@truman.edu>
2019-02-11 20:53:09 -0600

    fixed axis labels

--- 3Cycle/Presentation Pics/makeProps.R
+++ 3Cycle/Presentation Pics/makeProps.R
@@ -1,25 +1,25 @@
 greedyA = read.csv('fancy_greedy_DA.csv', header=FALSE, sep='\t')
 odasseA = read.csv('fwd_rep_odasse_DA.csv', header=FALSE, sep='\t')
 
-greedyO = subset(greedyA, grepl('I', V1) & V7 == 1)
+greedyO = subset(greedyA, grepl('I', V1) & V25 == .9)
 totalGreedyO = nrow(greedyO)
 propG_O_1 = nrow(subset(greedyO, V2 <=50))/totalGreedyO
 propG_O_2 = nrow(subset(greedyO, V2 ==51))/totalGreedyO
 propG_O_3 = nrow(subset(greedyO, V2 >51))/totalGreedyO
 
-greedyNO = subset(greedyA, grepl('I', V1) & V7 == 1)
+greedyNO = subset(greedyA, grepl('I', V1) & V25 != .9)
 totalGreedyNO = nrow(greedyNO)
 propG_NO_1 = nrow(subset(greedyNO, V2 <=50))/totalGreedyNO
 propG_NO_2 = nrow(subset(greedyNO, V2 ==51))/totalGreedyNO
 propG_NO_3 = nrow(subset(greedyNO, V2 >51))/totalGreedyNO
 
-odasseO = subset(odasseA, grepl('I', V1) & V8 == 1)
+odasseO = subset(odasseA, grepl('I', V1) & V26 == .9)
 totalOdasseO = nrow(odasseO)
 propO_O_1 = nrow(subset(odasseO, V2 <=50))/totalOdasseO
 propO_O_2 = nrow(subset(odasseO, V2 ==51))/totalOdasseO
 propO_O_3 = nrow(subset(odasseO, V2 >51))/totalOdasseO
 
-odasseNO = subset(odasseA, grepl('I', V1) & V8 == 1)
+odasseNO = subset(odasseA, grepl('I', V1) & V26 != .9)
 totalOdasseNO = nrow(odasseNO)
 propO_NO_1 = nrow(subset(odasseNO, V2 <=50))/totalOdasseNO
 propO_NO_2 = nrow(subset(odasseNO, V2 ==51))/totalOdasseNO
@@ -31,6 +31,6 @@ props = cbind(c(propG_O_1, propG_O_2, propG_O_3),
               c(propO_NO_1, propO_NO_2, propO_NO_3))
 
 
-par(mar=c(10,5,2,2), xpd=TRUE)
-barplot(props, col=c('ivory4', 'ivory3', 'ivory1'), names=c('Fancy Greedy O', 'fwd rep ODASSE O', 'Fancy Greedy NO', 'fwd rep ODASSE NO'), ylab='Proportion of Matches', main='Proportions of Matches by Algorithm and Recipient Blood Type')
+par(mar=c(10,5,2,2), xpd=TRUE, cex.lab=1, cex.main=.8)
+barplot(props, col=c('ivory4', 'ivory3', 'ivory1'), names=c('Fancy Greedy O', 'fwd rep ODASSE O', 'Fancy Greedy NO', 'fwd rep ODASSE NO'), ylab='Proportion of Matches', main='Proportions of Matches by Algorithm and Recipient PRA')
 legend('bottom',legend=c('Online', 'End', 'Unmatched'), fill=c('ivory4', 'ivory3', 'ivory2'), inset=c(-.4,-.8), ncol=3)
